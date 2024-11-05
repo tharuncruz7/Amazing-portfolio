067c1d7ce30ffca4187d485389b666a8723090d4
Tharun Tej <125950708+tharuncruz7@users.noreply.github.com>
2024-11-05 22:55:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text",{
-    strings:["Frontend Developer", "Python Developer", "Django Frameworke","Web Developer"],
+    strings:["Linux","Aws services","Devops tools like...","Ansible,Terraform,Docker,Kubernets,Jenkins","Python,Mysql"],
     typeSpeed : 10,
     backSpeed : 100,
     backDelay : 1000,
