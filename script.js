06e8310340dd0cc88227ba29b5c94557e110c326
Tharun Tej <125950708+tharuncruz7@users.noreply.github.com>
2024-11-05 17:06:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Python Developer", "Django Frameworke","Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
